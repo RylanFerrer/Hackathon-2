8285fa9adcf40cb021520d8745f67b4ae53176f4
Ivy Mageto <ivymageto@MirLeos-iPhone.localdomain>
2019-11-15 12:50:43 -0800

    made changes to states

--- client/src/components/Fightpage.js
+++ client/src/components/Fightpage.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import StatsPage from "./Statspage";
+import FightEvent from "./FightEvent";
 
 class Fightpage extends React.Component {
+  //this.props is how you access props
   render() {
     return (
       <section>
-        <h1>LET'S FIGHT!!</h1>
-        <button className="fight-button" type="submit">
-          FIGHT
-        </button>
+        <StatsPage stats={this.props.stats} />
+        <FightEvent health={this.props.health} />
       </section>
     );
   }
