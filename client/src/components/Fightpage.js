1b48b7268b6916aed7431d89dc9322ac4b2fadd3
Ivy Mageto <ivymageto@MirLeos-iPhone.localdomain>
2019-11-15 13:41:56 -0800

    made changes to main content

--- client/src/components/Fightpage.js
+++ client/src/components/Fightpage.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-import StatsPage from "./Statspage";
-import FightEvent from "./FightEvent";
+import Statspage from "./Statspage";
+// import FightEvent from "./FightEvent";
 
 class Fightpage extends React.Component {
   //this.props is how you access props
   render() {
     return (
       <section>
-        <StatsPage stats={this.props.stats} />
-        <FightEvent
+        <Statspage stats={this.props.stats} />
+        {/* <FightEvent
           health1={this.props.stats.health1}
           health2={this.props.stats.health2}
-        />
+        /> */}
       </section>
     );
   }
